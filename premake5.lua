29088e824c29075306e96b11ea1ef777cf9f73cf
Salt-M1ne <44857201+Salt-M1ne@users.noreply.github.com>
2020-01-26 10:49:43 +1300

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+		"src/**.h",
+		"src/**.cpp",
+		"src/**.inl",
+	}
+
+	includedirs
+	{
+		"include",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
